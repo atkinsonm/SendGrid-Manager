dcc6461f2eaf3e3075b8f23570fe84084ba63e6f
melusom <melusom2@tcnj.edu>
2016-07-06 16:52:00 -0400

    change to the display for no results

--- page-sendgrid-search-and-replace.php
+++ page-sendgrid-search-and-replace.php
@@ -1,76 +1,104 @@
 <?php
 /**
- * Template for displaying all pages
- *
- * This is a page to search all SendGrid recipient
- * lists for an email address and allow the user to
- * replace that address with another. Modified from 
- * page.php from Twenty Eleven 1.0. Written by Michael Meluso.
- * www.michaelmeluso.com
- *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
  */
-
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<div class="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
-
-                    
-					<?php
+<?php if (have_posts()): ?>
+	<?php while (have_posts()): the_post(); ?>
+	
+		<?php
                     require "sendgrid-get-lists.php";
                     require "sendgrid-add-email.php";
                     require "sendgrid-remove-email.php";
                     // SendGrid list custom code
                     // SendGrid Search and Replace
+					
                     if (empty($_POST)) {
-                        echo "<h1 class=\"entry-title\">Search All Lists for an Email Address</h1>";
-                        echo "<form id=\"form_search\" method=\"post\" action=\"".get_permalink()."\">";
-                        echo "<input type=\"text\" name=\"address\" placeholder=\"Email address\"><br>";
-                        echo "<input type=\"submit\" name=\"search\" value=\"Search\">";
-                        echo "</form>";
+					?>
+                        <h1 class="entry-title">Search All Lists for an Email Address</h1>
+                        <form id="form_search" method="post" action="<?php echo get_permalink() ?>">
+                        <input type="text" name="address" placeholder="Email address"><br>
+                        <input type="submit" name="search" value="Search">
+                        </form>
+					<?php
                     } else if (isset($_POST['search'])) {
                         $lists = getLists(null);
                         $hits = "";
                         $email = "";
                         foreach($_POST as $name => $value) {
                             if ($name == "address") {
-                                $email = $value;
-                                echo "<h1 class=\"entry-title\">Search Results for \"$email\"</h1>";
-                                echo $email . " was found in the following lists:<br>";
-                                echo "<form id=\"form_remove\" method=\"post\" action=\"".get_permalink()."\">";
-                                echo "<table class=\"recip-table\">";
-                                echo "<tr class = \"recip-table-top\">";
-                                echo "<th><strong>Recipient List</strong></th>";
-                                echo "<th><strong>Remove</strong></th>";
-                                echo "</tr>";
+                                $email = $value; ?>
+                                <h1 class="entry-title">Search Results for "<?php echo $email ?>"</h1>
+							<?php
+								$deepsearch = true;
                                 foreach ($lists as $value) {
                                     $value = substr($value, 0, strlen($value)-1);
                                     $result = getEmails($value, $email);
-                                    if (!(is_null($result))) {
-                                        $valueF = str_replace(' ', '-', $result);
+									
+									if (is_null($result) && $deepsearch == true) { // Perform a deep search
+										$listContents = getEmails($value);
+										$result = array_filter($listContents, function($var) use ($email) { return preg_match("/\b$email\b/i", $var); });
+										
+										if (!(is_null($result))) { // If is still null, then none found
+											foreach ($result as $match) {
+												$valueF = str_replace(' ', '-', $value);
+                                        		$hits = $hits . "," . $valueF;
+											?>
+												<?php echo $email ?> was found in the following lists:<br>
+                                				<table class="recip-table">
+                                				<tr class = "recip-table-top">
+                                				<th><strong>Recipient List</strong></th>
+                                				<th><strong>Remove</strong></th>
+                                				</tr>
+                                        		<tr class="recip-table-tr">
+												<form id="form_search" method="post" action="<?php echo get_permalink() ?>">
+                                        		<td><?php echo $match ?><input type="hidden" name="address" value=<?php echo $match ?>></td>
+                                        		<td><input type="submit" name="search" value="Search again for <?php echo $match ?>"></td>
+												</form>
+												</tr>
+											<?php
+											}
+										} else {
+											<?php echo $email ?> was not found in any lists.<br>
+										}
+										
+									} else if (!is_null($result)) { // Found using SendGrid search
+										$valueF = str_replace(' ', '-', $result);
                                         $hits = $hits . "," . $valueF;
-                                        echo "<tr class=\"recip-table-tr\">";
-                                        echo "<td class=\"recip-table-td\">$result<input type=\"hidden\" name=\"email\" value=$email></td>";
-                                        echo "<td class=\"recip-table-td\"><input type=\"submit\" name=$valueF value=\"Remove from $result \"></td>";
-                                        echo "</tr>";
-                                    }
+										$deepsearch = false;
+									?>
+                                        <tr class="recip-table-tr">
+										<form id="form_remove" method="post" action="<?php echo get_permalink() ?>">
+                                        <td><?php echo $result ?><input type="hidden" name="email" value=<?php echo $email ?>></td>
+                                        <td><input type="submit" name="<?php echo $valueF ?>" value="Remove from <?php echo $result ?>"></td>
+										</form>
+                                        </tr>
+									<?php
+									}
                                 }
                             }
-                        }
-                        echo "</table>";
-                        echo "</form>";
-                        echo "Replace all instances with:<br>";
-                        echo "<form id=\"form_replace\" method=\"post\" action=\"".get_permalink()."\">";
-                        echo "<input type=\"hidden\" name=\"old\" value=$email>";
-                        echo "<input type=\"text\" name=\"new\" placeholder=\"Email address\"><br>";
-                        echo "<input type=\"hidden\" name=\"hits\" value=$hits>";
-                        echo "<input type=\"submit\" name=\"replace\" value=\"Replace\"><br>";
-                        echo "</form>";
+                        } ?>
+                        </table>
+						<br>
+					<?php
+						if (!$deepsearch) {
+						?>
+                        Replace all instances with:<br>
+                        <form id="form_replace" method="post" action="<?php echo get_permalink() ?>">
+                        <input type="hidden" name="old" value=<?php echo $email ?>>
+                        <input type="text" name="new" placeholder="Email address"><br>
+                        <input type="hidden" name="hits" value=<?php echo $hits ?>>
+                        <input type="submit" name="replace" value="Replace"><br>
+                        </form>
+					
+					<?php
+						} else { ?>
+							<strong>Please search again for a specific email address to use the search-and-replace or remove features.</strong>
+					<?php
+						}
                     } else if (isset($_POST['replace'])) {
                         echo "<h1 class=\"entry-title\">Replace</h1>";
                         $listStr = $_POST['hits'];
@@ -87,26 +115,28 @@ get_header(); ?>
                         }
                     } else if (isset($_POST)) {
                         echo "<h1 class=\"entry-title\">Removing</h1>";
-                        foreach($_POST as $name => $value) {
-                            if ($name == "email") {
+                        foreach($_POST as $name => $value) {                   
+			    if ($name == "email") {
                                 $email = $value;
                             } else {
                                 $valueF = str_replace('-', ' ', $value);
                                 $list = substr($valueF, 12);
-                                $list = substr($list, 0, strlen($list)-1);
                                 $resultMsg = removeFromList($email, $list);
                                 echo $resultMsg . "<br>";
                             }
                         }
-                    }
-                    echo "<br><a href=\"http://www.solec.org/index.php/sendgrid-manager/\">Back to SendGrid Manager</a>";
-                    ?>
+                    }?>
+                    <br><a href="<?php bloginfo('wpurl'); ?>/sendgrid-manager/">Back to SendGrid Manager</a>
+		
+	<?php endwhile; ?>
+	
+<?php else: ?>
 
-					<?php comments_template( '', true ); ?>
+    <?php get_template_part('notfound'); ?>
+    
+<?php endif; ?>
 
-				<?php endwhile; // end of the loop. ?>
+</div><!--END content-->
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
